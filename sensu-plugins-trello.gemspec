d89b87bf843362e4be60ae319c753c3f63f2b323
Stefan Walluhn <stefan.walluhn@aboutsource.net>
2018-03-23 10:50:38 +0100

    Test: introduce webmock

--- sensu-plugins-trello.gemspec
+++ sensu-plugins-trello.gemspec
@@ -24,4 +24,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rspec', '~> 3.7'
   spec.add_development_dependency 'rubocop', '~> 0.54'
+  spec.add_development_dependency 'webmock', '~> 3.3'
 end
--- test/check-trello-incidents_spec.rb
+++ test/check-trello-incidents_spec.rb
@@ -1,12 +1,21 @@
+require 'webmock/rspec'
+
 require_relative './plugin_stub.rb'
 require_relative '../bin/check-trello-incidents.rb'
 
 describe CheckTrelloIncidents do
   it 'should run' do
+    stub_request(
+      :get,
+      'https://api.trello.com//1/lists/abcde/cards/?key=12345&token=12345'
+    ).to_return(body: '[]', status: 200)
+
     check = CheckTrelloIncidents.new
     check.config[:api_key] = '12345'
     check.config[:api_token] = '12345'
     check.config[:list] = 'abcde'
-    expect(-> { check.run }).to raise_error Net::HTTPServerException
+
+    expect(check).to receive(:output).with('No new incidents')
+    expect(-> { check.run }).to raise_error SystemExit
   end
 end
